84d6485443d264ebef4f93a3ce0833d46285317c
cwils132 <cw1621@ship.edu>
2016-09-25 21:58:32 -0400

    Updated: WorldController, WorldRenderer, CameraHelper, Constants
    Updated classes to use images in spritesheet and properly display GUI

--- core/src/com/wilson/gdx/util/Constants.java
+++ core/src/com/wilson/gdx/util/Constants.java
@@ -11,11 +11,20 @@ public class Constants
 
 	// Visible game world is 5 meters tall
 	public static final float VIEWPORT_HEIGHT = 5.0f;
+	
+	// GUI Width
+	public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+
+	// GUI Height
+	public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
 
 	// Location of description file for texture atlas
-	public static final String TEXTURE_ATLAS_OBJECTS = "/wilson-gdx-game-desktop/assets-raw/images/canyonbunny.pack.atlas";
+	public static final String TEXTURE_ATLAS_OBJECTS = "../core/assets/images/thelibrary.pack.atlas";
 
 	// Location of image file for level 01
-	public static final String LEVEL_01 = "levels/level-01.png";
+	public static final String LEVEL_01 = "../core/assets/images/level-01.png";
+	
+	// Amount of extra lives at level start
+	public static final int LIVES_START = 3;
 
 }
