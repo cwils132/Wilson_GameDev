fadc480e80d4882a1371413879b3017596cb57c8
cwils132 <cw1621@ship.edu>
2016-10-02 16:57:06 -0400

    Assignment 6: Added AbstractGameObject
    Class holds global values for all objects in game

--- core/src/com/wilson/gdx/game/objects/AbstractGameObject.java
+++ core/src/com/wilson/gdx/game/objects/AbstractGameObject.java
@@ -2,14 +2,16 @@ package com.wilson.gdx.game.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.MathUtils;
 
 /**
- * We are using this abstract class to store basic information
- * about our game objects without any direct alteration
- * to their functions.
+ * We are using this abstract class to store basic information about our game
+ * objects without any direct alteration to their functions.
  * 
- * Here we store info about position, dimension, origin, scale factor,
- * and angle of rotation.
+ * Here we store info about position, dimension, origin, scale factor, and angle
+ * of rotation.
+ * 
  * @author Chris
  *
  */
@@ -21,6 +23,13 @@ public abstract class AbstractGameObject
 	public Vector2 scale;
 	public float rotation;
 
+	public Vector2 velocity;
+	public Vector2 terminalVelocity;
+	public Vector2 friction;
+
+	public Vector2 acceleration;
+	public Rectangle bounds;
+
 	public AbstractGameObject()
 	{
 		position = new Vector2();
@@ -28,20 +37,70 @@ public abstract class AbstractGameObject
 		origin = new Vector2();
 		scale = new Vector2(1, 1);
 		rotation = 0;
+		velocity = new Vector2();
+		terminalVelocity = new Vector2(1, 1);
+		friction = new Vector2();
+		acceleration = new Vector2();
+		bounds = new Rectangle();
 	}
 
 	/**
-	 * These classes exist so that World Controller can call on it
-	 * (as it really wants to do) but will not actually alter
-	 * our objects. Because each object will react
-	 * to situations differently, we will define these
-	 * behaviors in the objects themselves.
+	 * These classes exist so that World Controller can call on it (as it really
+	 * wants to do) but will not actually alter our objects. Because each object
+	 * will react to situations differently, we will define these behaviors in
+	 * the objects themselves.
+	 * 
 	 * @param deltaTime
 	 */
 	public void update(float deltaTime)
 	{
+		updateMotionX(deltaTime);
+		updateMotionY(deltaTime);
+		// Move to new position
+		position.x += velocity.x * deltaTime;
+		position.y += velocity.y * deltaTime;
 	}
 
 	public abstract void render(SpriteBatch batch);
+
+	protected void updateMotionX(float deltaTime)
+	{
+		if (velocity.x != 0)
+		{
+			// Apply friction
+			if (velocity.x > 0)
+			{
+				velocity.x = Math.max(velocity.x - friction.x * deltaTime, 0);
+			} else
+			{
+				velocity.x = Math.min(velocity.x + friction.x * deltaTime, 0);
+			}
+		}
+		// Apply acceleration
+		velocity.x += acceleration.x * deltaTime;
+		// Make sure the object's velocity does not exceed the
+		// positive or negative terminal velocity
+		velocity.x = MathUtils.clamp(velocity.x, -terminalVelocity.x, terminalVelocity.x);
+	}
+
+	protected void updateMotionY(float deltaTime)
+	{
+		if (velocity.y != 0)
+		{
+			// Apply friction
+			if (velocity.y > 0)
+			{
+				velocity.y = Math.max(velocity.y - friction.y * deltaTime, 0);
+			} else
+			{
+				velocity.y = Math.min(velocity.y + friction.y * deltaTime, 0);
+			}
+		}
+		// Apply acceleration
+		velocity.y += acceleration.y * deltaTime;
+		// Make sure the object's velocity does not exceed the
+		// positive or negative terminal velocity
+		velocity.y = MathUtils.clamp(velocity.y, -terminalVelocity.y, terminalVelocity.y);
+	}
 
 }
