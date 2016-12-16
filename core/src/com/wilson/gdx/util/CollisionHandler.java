5257a0ebbcb3f46b979531407871cc089d57f920
cwils132 <cw1621@ship.edu>
2016-12-16 05:26:18 -0500

    Finnal: Update: CollisionHandler updated with new conditionals
    Class updated to handle all objects in game world as well as manage item actions during contact.

--- core/src/com/wilson/gdx/util/CollisionHandler.java
+++ core/src/com/wilson/gdx/util/CollisionHandler.java
@@ -9,183 +9,231 @@ import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.wilson.gdx.game.WorldController;
 import com.wilson.gdx.game.Assets;
+import com.wilson.gdx.game.Emerald;
 import com.wilson.gdx.game.objects.AbstractGameObject;
 import com.wilson.gdx.game.objects.Rock;
+import com.wilson.gdx.game.objects.RoughRock;
 import com.wilson.gdx.game.objects.BunnyHead;
+import com.wilson.gdx.game.objects.Feather;
 //import com.wilson.gdx.game.objects.BunnyHead.JUMP_STATE;
 import com.wilson.gdx.game.objects.GoldCoin;
+import com.wilson.gdx.game.objects.Portal;
 import com.wilson.gdx.game.Level;
 
-
 /**
- * This class is to handle all collisions for Box2D. We only need to worry about Rocks'
- * and the Player at the moment so that is all it will be checking against.
+ * This class is to handle all collisions for Box2D. We only need to worry about
+ * Rocks' and the Player at the moment so that is all it will be checking
+ * against.
+ * 
  * @author Chris
  *
  */
 public class CollisionHandler implements ContactListener
 {
-    private ObjectMap<Short, ObjectMap<Short, ContactListener>> listeners;
+	private ObjectMap<Short, ObjectMap<Short, ContactListener>> listeners;
 
-    private WorldController world;
+	private WorldController world;
+	GamePreferences preferences = GamePreferences.instance;
 
-    public CollisionHandler(WorldController w)
-    {
-    	world = w;
-        listeners = new ObjectMap<Short, ObjectMap<Short, ContactListener>>();
-    }
+	public CollisionHandler(WorldController w)
+	{
+		world = w;
+		listeners = new ObjectMap<Short, ObjectMap<Short, ContactListener>>();
+	}
 
-    /**
-     * Adds listeners to check if contact occurs between the Player
-     * and another object.
-     * @param categoryA
-     * @param categoryB
-     * @param listener
-     */
-    public void addListener(short categoryA, short categoryB, ContactListener listener)
-    {
-        addListenerInternal(categoryA, categoryB, listener);
-        addListenerInternal(categoryB, categoryA, listener);
-    }
+	/**
+	 * Adds listeners to check if contact occurs between the Player and another
+	 * object.
+	 * 
+	 * @param categoryA
+	 * @param categoryB
+	 * @param listener
+	 */
+	public void addListener(short categoryA, short categoryB, ContactListener listener)
+	{
+		addListenerInternal(categoryA, categoryB, listener);
+		addListenerInternal(categoryB, categoryA, listener);
+	}
 
-    /**
-     * If a contact occurs it is handeled.
-     */
-    @Override
-    public void beginContact(Contact contact)
-    {
-        Fixture fixtureA = contact.getFixtureA();
-        Fixture fixtureB = contact.getFixtureB();
+	/**
+	 * If a contact occurs it is handeled.
+	 */
+	@Override
+	public void beginContact(Contact contact)
+	{
+		Fixture fixtureA = contact.getFixtureA();
+		Fixture fixtureB = contact.getFixtureB();
 
-        //Gdx.app.log("CollisionHandler-begin A", "begin");
+		// Gdx.app.log("CollisionHandler-begin A", "begin");
 
-       // processContact(contact);
-        processContact(contact);
+		// processContact(contact);
+		processContact(contact);
 
-        ContactListener listener = getListener(fixtureA.getFilterData().categoryBits, fixtureB.getFilterData().categoryBits);
-        if (listener != null)
-        {
-            listener.beginContact(contact);
-        }
-    }
+		ContactListener listener = getListener(fixtureA.getFilterData().categoryBits,
+		        fixtureB.getFilterData().categoryBits);
+		if (listener != null)
+		{
+			listener.beginContact(contact);
+		}
+	}
 
-    /**
-     * Resolves contacts between objects
-     */
-    @Override
-    public void endContact(Contact contact)
-    {
-        Fixture fixtureA = contact.getFixtureA();
-        Fixture fixtureB = contact.getFixtureB();
+	/**
+	 * Resolves contacts between objects
+	 */
+	@Override
+	public void endContact(Contact contact)
+	{
+		Fixture fixtureA = contact.getFixtureA();
+		Fixture fixtureB = contact.getFixtureB();
 
-       // Gdx.app.log("CollisionHandler-end A", "end");
+		// Gdx.app.log("CollisionHandler-end A", "end");
 
+		// Gdx.app.log("CollisionHandler-end A",
+		// fixtureA.getBody().getLinearVelocity().x+" :
+		// "+fixtureA.getBody().getLinearVelocity().y);
+		// Gdx.app.log("CollisionHandler-end B",
+		// fixtureB.getBody().getLinearVelocity().x+" :
+		// "+fixtureB.getBody().getLinearVelocity().y);
+		ContactListener listener = getListener(fixtureA.getFilterData().categoryBits,
+		        fixtureB.getFilterData().categoryBits);
+		if (listener != null)
+		{
+			listener.endContact(contact);
+		}
+	}
 
-        // Gdx.app.log("CollisionHandler-end A", fixtureA.getBody().getLinearVelocity().x+" : "+fixtureA.getBody().getLinearVelocity().y);
-        // Gdx.app.log("CollisionHandler-end B", fixtureB.getBody().getLinearVelocity().x+" : "+fixtureB.getBody().getLinearVelocity().y);
-        ContactListener listener = getListener(fixtureA.getFilterData().categoryBits, fixtureB.getFilterData().categoryBits);
-        if (listener != null)
-        {
-            listener.endContact(contact);
-        }
-    }
+	@Override
+	public void preSolve(Contact contact, Manifold oldManifold)
+	{
+		Fixture fixtureA = contact.getFixtureA();
+		Fixture fixtureB = contact.getFixtureB();
+		ContactListener listener = getListener(fixtureA.getFilterData().categoryBits,
+		        fixtureB.getFilterData().categoryBits);
+		if (listener != null)
+		{
+			listener.preSolve(contact, oldManifold);
+		}
+	}
 
-    @Override
-    public void preSolve(Contact contact, Manifold oldManifold)
-    {
-        Fixture fixtureA = contact.getFixtureA();
-        Fixture fixtureB = contact.getFixtureB();
-        ContactListener listener = getListener(fixtureA.getFilterData().categoryBits, fixtureB.getFilterData().categoryBits);
-        if (listener != null)
-        {
-            listener.preSolve(contact, oldManifold);
-        }
-    }
+	@Override
+	public void postSolve(Contact contact, ContactImpulse impulse)
+	{
+		Fixture fixtureA = contact.getFixtureA();
+		Fixture fixtureB = contact.getFixtureB();
+		ContactListener listener = getListener(fixtureA.getFilterData().categoryBits,
+		        fixtureB.getFilterData().categoryBits);
+		if (listener != null)
+		{
+			listener.postSolve(contact, impulse);
+		}
+	}
 
-    @Override
-    public void postSolve(Contact contact, ContactImpulse impulse)
-    {
-        Fixture fixtureA = contact.getFixtureA();
-        Fixture fixtureB = contact.getFixtureB();
-        ContactListener listener = getListener(fixtureA.getFilterData().categoryBits, fixtureB.getFilterData().categoryBits);
-        if (listener != null)
-        {
-            listener.postSolve(contact, impulse);
-        }
-    }
+	private void addListenerInternal(short categoryA, short categoryB, ContactListener listener)
+	{
+		ObjectMap<Short, ContactListener> listenerCollection = listeners.get(categoryA);
+		if (listenerCollection == null)
+		{
+			listenerCollection = new ObjectMap<Short, ContactListener>();
+			listeners.put(categoryA, listenerCollection);
+		}
+		listenerCollection.put(categoryB, listener);
+	}
 
-    private void addListenerInternal(short categoryA, short categoryB, ContactListener listener)
-    {
-        ObjectMap<Short, ContactListener> listenerCollection = listeners.get(categoryA);
-        if (listenerCollection == null)
-        {
-            listenerCollection = new ObjectMap<Short, ContactListener>();
-            listeners.put(categoryA, listenerCollection);
-        }
-        listenerCollection.put(categoryB, listener);
-    }
+	private ContactListener getListener(short categoryA, short categoryB)
+	{
+		ObjectMap<Short, ContactListener> listenerCollection = listeners.get(categoryA);
+		if (listenerCollection == null)
+		{
+			return null;
+		}
+		return listenerCollection.get(categoryB);
+	}
 
-    private ContactListener getListener(short categoryA, short categoryB)
-    {
-        ObjectMap<Short, ContactListener> listenerCollection = listeners.get(categoryA);
-        if (listenerCollection == null)
-        {
-            return null;
-        }
-        return listenerCollection.get(categoryB);
-    }
+	/**
+	 * Processes contact within game.
+	 * 
+	 * @param contact
+	 */
+	private void processContact(Contact contact)
+	{
+		Fixture fixtureA = contact.getFixtureA();
+		Fixture fixtureB = contact.getFixtureB();
+		AbstractGameObject objA = (AbstractGameObject) fixtureA.getBody().getUserData();
+		AbstractGameObject objB = (AbstractGameObject) fixtureB.getBody().getUserData();
 
-    /**
-     * Processes contact within game.
-     * @param contact
-     */
-    private void processContact(Contact contact)
-    {
-    	Fixture fixtureA = contact.getFixtureA();
-        Fixture fixtureB = contact.getFixtureB();
-        AbstractGameObject objA = (AbstractGameObject)fixtureA.getBody().getUserData();
-        AbstractGameObject objB = (AbstractGameObject)fixtureB.getBody().getUserData();
+		if (objA instanceof BunnyHead)
+		{
+			processPlayerContact(fixtureA, fixtureB);
+		} else if (objB instanceof BunnyHead)
+		{
+			processPlayerContact(fixtureB, fixtureA);
+		}
+	}
 
-        if (objA instanceof BunnyHead)
-        {
-        	processPlayerContact(fixtureA, fixtureB);
-        }
-        else if (objB instanceof BunnyHead)
-        {
-        	processPlayerContact(fixtureB, fixtureA);
-        }
-    }
+	/**
+	 * Handles all collisions and and boolean values
+	 * related to movement in the game world. Also plays sound effects
+	 * if applicable.
+	 * 
+	 * Instructs items when they have been collected and flags them for
+	 * removal.
+	 * 
+	 * @param playerFixture
+	 * @param objFixture
+	 */
+	private void processPlayerContact(Fixture playerFixture, Fixture objFixture)
+	{
+		if (objFixture.getBody().getUserData() instanceof Rock)
+		{
+			world.resetJump();
+			world.level.bunnyHead.grounded = true;
+			world.level.bunnyHead.jumping = false;
+		}
+		
+		else if (objFixture.getBody().getUserData() instanceof RoughRock)
+		{
+			world.resetJump();
+			world.level.bunnyHead.grounded = true;
+			world.level.bunnyHead.jumping = false;
+		}
 
-    /**
-     * If player comes in contact with the rocks the jump state is set to grounded
-     * If the player picks up a coin, jumps, or loses  a life, a sound is played.
-     * @param playerFixture
-     * @param objFixture
-     */
-    private void processPlayerContact(Fixture playerFixture, Fixture objFixture)
-    {
-        if (objFixture.getBody().getUserData() instanceof Rock)
-        {
-        	Gdx.app.error("CH", "Hit Rock");
-        	world.resetJump();
-        	world.level.bunnyHead.grounded = true;
-        	world.level.bunnyHead.jumping = false;
-        	if (world.level.bunnyHead.jumping){
-        		System.out.println("Jumping");
-        	}
-        }
+		else if (objFixture.getBody().getUserData() instanceof GoldCoin)
+		{
+			GoldCoin book = (GoldCoin) objFixture.getBody().getUserData();
+			if (!book.collected)
+			{
+				AudioManager.instance.play(Assets.instance.sounds.pickupCoin);
+			}
 
-    	else if (objFixture.getBody().getUserData() instanceof GoldCoin)
-        {
-    		GoldCoin book = (GoldCoin) objFixture.getBody().getUserData();
-    		if (!book.collected){
-    			AudioManager.instance.play(Assets.instance.sounds.pickupCoin);
-    		}
+			book.collected = true;
+			world.score += book.getScore();
+			world.flagForRemoval(book);
+		}
+		else if (objFixture.getBody().getUserData() instanceof Feather)
+		{
+			Feather feather = (Feather) objFixture.getBody().getUserData();
+			if (!feather.collected)
+			{
+				AudioManager.instance.play(Assets.instance.sounds.pickupFeather);
+				world.level.bunnyHead.setFeatherPowerup(true);
+			}
 
-            book.collected = true;
-            world.score += book.getScore();
-            world.flagForRemoval(book);
-        }
-    }
+			feather.collected = true;
+			world.score += feather.getScore();
+			world.flagForRemoval(feather);
+		}
+		else if (objFixture.getBody().getUserData() instanceof Emerald)
+		{
+			Emerald emerald = (Emerald) objFixture.getBody().getUserData();
+			if (!emerald.collected)
+			{
+				AudioManager.instance.play(Assets.instance.sounds.pickupFeather);
+				world.level.bunnyHead.setEmeraldPowerup(true);
+			}
 
+			emerald.collected = true;
+			world.score += emerald.getScore();
+			world.flagForRemoval(emerald);
+		}
+	}
 }
